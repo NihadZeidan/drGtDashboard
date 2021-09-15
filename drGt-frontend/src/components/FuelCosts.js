2909fadc5e1e1bda1969a378e397b773a590a34b
Nihad Emad Zeidan <nihadzeidan@gmail.com>
2021-09-15 16:55:53 +0300

    adding dotenv variable

--- drGt-frontend/src/components/FuelCosts.js
+++ drGt-frontend/src/components/FuelCosts.js
@@ -9,6 +9,7 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
 
 function FuelCosts() {
@@ -32,8 +33,8 @@ function FuelCosts() {
   // Calculate the total meters consumed
   let totalMeters = fuelCosts.monthsSummery
     ? fuelCosts.monthsSummery.reduce((acc, month) => {
-        return (acc += month.uv);
-      }, 0)
+      return (acc += month.uv);
+    }, 0)
     : null;
   // Calculate total needed liters by divide total meters consumed / cars consumption per liter
   let averageFuelConsumption = (
@@ -65,22 +66,25 @@ function FuelCosts() {
           <p className="content"> $70.000</p>
         </p>
       </div>
-      <AreaChart
-        width={700}
-        height={350}
-        data={displayData}
-        margin={{
-          top: 10,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#4CAB9A" fill="#E2F1EF" />
-      </AreaChart>
+      <ResponsiveContainer width={700}
+        height={350}>
+
+        <AreaChart
+
+          data={displayData}
+          margin={{
+            top: 10,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stroke="#4CAB9A" fill="#E2F1EF" />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 }
